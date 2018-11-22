46406416ca8770ee2a5edccf3b9300fbf1b50161
gustavo <gustavopetruzzi19@gmail.com>
2018-11-22 10:08:32 -0300

    Arreglo error cuando no hay mozo y agrego pipe  Changes to be committed: 	modified:   src/pages/reservas/reservas-mestre/reservas-mestre.html 	modified:   src/pages/reservas/reservas-mestre/reservas-mestre.module.ts 	modified:   src/pages/reservas/reservas-mestre/reservas-mestre.ts 	new file:   src/pipes/fecha/fecha.ts 	new file:   src/pipes/pipes.module.ts

--- src/pages/reservas/reservas-mestre/reservas-mestre.module.ts
+++ src/pages/reservas/reservas-mestre/reservas-mestre.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ReservasMestrePage } from './reservas-mestre';
-
+import {PipesModule} from '../../../pipes/pipes.module';
 @NgModule({
   declarations: [
     ReservasMestrePage,
   ],
   imports: [
     IonicPageModule.forChild(ReservasMestrePage),
+    PipesModule
   ],
 })
 export class ReservasMestrePageModule {}
